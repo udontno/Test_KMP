5195d5515e247dde07b0eb7f6e6516568f706671
Udontno <1459892307@qq.com>
2025-09-02 17:29:42 +0800

    添加spmForKmp插件使用示例

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # KMP项目-基础使用
+## spmForKmp插件使用-pod库依赖(branch:use_spmForKmp)
+- 线上framework仓库
+- 本地framework仓库
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    id("io.github.frankois944.spmForKmp") version "1.0.0-Beta03"
 }
 
 kotlin {
@@ -19,7 +20,7 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -29,10 +30,17 @@ kotlin {
             baseName = "ComposeApp"
             isStatic = true
         }
+
+        iosTarget.compilations {
+            val main by getting {
+                // Choose the cinterop name
+                cinterops.create("nativeBridge")
+            }
+        }
     }
-    
+
     jvm()
-    
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         outputModuleName.set("composeApp")
@@ -52,7 +60,7 @@ kotlin {
         }
         binaries.executable()
     }
-    
+
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
@@ -118,6 +126,59 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "org.ud.pro"
             packageVersion = "1.0.0"
+        }
+    }
+}
+
+
+swiftPackageConfig {
+    create("nativeBridge") { // 必须和上面cinterops.create("nativeBridge")内容一样，不然会报错list empty
+        // optional parameters
+        // the ios minimal version
+        // minIos = "12.0"
+        // the tvos minimal version
+        // minTvos = "12.0"
+        // the watchos minimal version
+        // minWatchos = "12.0"
+        // the macos minimal version
+        // minMacos = "10.15"
+        // the directory where your own swift code is located
+        // customPackageSourcePath = "{buildDir}/src/swift"
+        // the swift code is built in debug by default
+        // debug = false :
+        // add a prefix to the dependencies package names
+        // ei :
+        //  - packageDependencyPrefix = "customName"
+        //  - give : "customName.FirebaseCore" instead of "FirebaseCore"
+        // packageDependencyPrefix = null // default null
+        spmWorkingPath =
+            "${projectDir.resolve("SPM")}" // change the Swift Package Manager working Dir
+        // swiftBinPath = "/path/to/.swiftly/bin/swift"
+        minIos = "16.0"
+        dependency {
+            // ✅正常
+            remoteBinary(
+                url = uri("https://github.com/wanliyunyan/LibXray/releases/download/25.8.3/LibXray.xcframework.zip"),
+                packageName = "LibXray",
+                exportToKotlin = true,
+                checksum = "1f478c370bbe9cce12ed7f2f6101853a74c3a2220253dbaf5abcc446260c393d",
+            )
+            // ✅正常
+//            localBinary(
+//                path = "/Users/udontno/Downloads/LibXray.xcframework.zip",
+//                packageName = "LibXray",
+//                exportToKotlin = true
+//            )
+
+
+            // ❌报错，库型xcframework需要使用手动依赖方式，参考分支dependence_library_xcframework
+//            remoteBinary(
+//                url = uri("https://github.com/wanliyunyan/HevSocks5Tunnel/releases/download/2.10.0/HevSocks5Tunnel.xcframework.zip"),
+//                packageName = "HevSocks5Tunnel",
+//                exportToKotlin = true,
+//                checksum = "f66fc314edbdb7611c5e8522bc50ee62e7930f37f80631b8d08b2a40c81a631a",
+//            )
+
         }
     }
 }
